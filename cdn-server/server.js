f1a8d5b07442b3e983fdbe24d16a5bc65cd923bb
Zerogravy <zerogravityexplorer@gmail.com>
2023-09-25 12:12:00 +0530

    added AI code for analysing the data

--- cdn-server/server.js
+++ cdn-server/server.js
@@ -4,6 +4,7 @@ const cors = require("cors"); // Import the cors middleware
 const app = express();
 const port = 3000; // You can change this to your desired port
 const axios = require("axios");
+const { Configuration, OpenAIApi } = require("openai");
 
 // Use body-parser middleware to parse JSON requests
 app.use(bodyParser.json());
@@ -14,31 +15,78 @@ app.use(cors());
 // Store received data in an array (for demonstration purposes)
 
 // Route to receive data from your library
-app.post("/receive-data", (req, res) => {
-  const data = req.body;
-  receivedData = data;
-  console.log("Received Data:", data);
+app.post("/receive-data", async (req, res) => {
+  try {
+    const data = req.body;
+    receivedData = data;
+    console.log("Received Data:", data);
+    const apiKey = "UXHQRSK8A2TFSAZP1SGQD1SGFSPXCRFRDI"; // Replace with your Bttcscan API key
 
-  const apiKey = "UXHQRSK8A2TFSAZP1SGQD1SGFSPXCRFRDI"; // Replace with your Bttcscan API key
-  const contractAddress = receivedData.contractAddress; // Replace with your dynamic address
+    // All details required to analyze the contract
+    const contractAddress = receivedData.contractAddress; // Replace with your dynamic address
+    let sourceCode = {};
+    const input = receivedData.inputValue;
+    const params = receivedData.params;
+    const functionName = receivedData.functionName;
+    let contractDetails = {};
 
-  const apiUrl = `https://api-testnet.bttcscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${apiKey}`;
-  console.log(apiUrl);
-  axios
-    .get(apiUrl)
-    .then((response) => {
-      // Handle the API response here
-      console.log(response.data.result[0].SourceCode);
-      res.status(200).send({
-        message: "Data received successfully",
-        receivedData: response.data.result[0].SourceCode,
-      });
-    })
-    .catch((error) => {
-      // Handle errors here
-      console.error(error);
+    const apiUrl = `https://api-testnet.bttcscan.com/api?module=contract&action=getsourcecode&address=${contractAddress}&apikey=${apiKey}`;
+
+    // Use async/await to make the API call
+    const response = await axios.get(apiUrl);
+
+    // Handle the API response here
+    console.log("Contract Data:", response.data);
+    sourceCode = response.data.result[0].SourceCode;
+    contractDetails = {
+      input: input,
+      params: params,
+      functionName: functionName,
+      sourceCode: sourceCode,
+    };
+
+    // Function call to get the NLP
+    let result = await explainSmartContract(contractDetails);
+    console.log(result);
+
+    // Send the response
+    res.status(200).send({
+      message: "Data received successfully",
+      receivedData: result,
     });
+  } catch (error) {
+    // Handle errors here
+    console.error(error);
+    res.status(500).send({ error: "An error occurred" });
+  }
 });
+
+async function explainSmartContract(contractDetails) {
+  const configuration = new Configuration({
+    apiKey: "",
+  });
+  const openai = new OpenAIApi(configuration);
+  const messages = [
+    {
+      role: "system",
+      content:
+        "Explain what the contract call will do with the functionName, inputs and params called, from the source code.",
+    },
+    { role: "user", content: contractDetails },
+  ];
+  try {
+    const response = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: messages,
+      temperature: 0,
+      max_tokens: 1024,
+    });
+    console.log(response.data.choices[0].message.content);
+    return response.data.choices[0].message.content;
+  } catch (error) {
+    console.log("erros is the ", error);
+  }
+}
 
 // Start the server
 app.listen(port, () => {
