ef99d70dde952e6c987dc0ca89103223a2551c66
Zerogravy <zerogravityexplorer@gmail.com>
2023-09-28 08:53:16 +0530

    update popup css

--- ascai-trx/src/Popup.js
+++ ascai-trx/src/Popup.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { useState } from "react";
 import "./popup.css";
 
 function Popup(props) {
@@ -11,15 +12,37 @@ function Popup(props) {
       <div class="popup-overlay">
         <div class="popup-container">
           <div class="popup-card">
-            <h2>Are you sure you want to proceed?</h2>
-            <p id="popUpData"></p>
-            <button id="approve" onClick={() => props.approve()}>
-              Approve
-            </button>
-
-            <button id="close-popup" onClick={closePopup}>
-              Cancel
-            </button>
+            <h2 className="popup-head">Are you sure you want to proceed?</h2>
+            <div style={{ margin: "50px auto" }}>
+              <div className="data-flex">
+                <div className="data-lable">NLPGeneratedDescription: </div>
+                <div id="popUpData1" className="data-value"></div>
+              </div>
+              <div className="data-flex">
+                <div className="data-lable">InputParameters: </div>
+                <div id="popUpData2" className="data-value"></div>
+              </div>
+              <div className="data-flex">
+                <div className="data-lable">SmartContractCode: </div>
+                <div id="popUpData3" className="data-value"></div>
+              </div>
+              <div className="data-flex">
+                <div className="data-lable">FunctionName: </div>
+                <div id="popUpData4" className="data-value"></div>
+              </div>
+              <div className="data-flex">
+                <div className="data-lable">ContractSafetyScore: </div>
+                <div id="popUpData5" className="data-value"></div>
+              </div>
+            </div>
+            <div style={{ display: "flex", justifyContent: "center" }}>
+              <button id="approve" onClick={() => props.approve()}>
+                Approve
+              </button>
+              <button id="close-popup" onClick={closePopup}>
+                Cancel
+              </button>
+            </div>
           </div>
         </div>
       </div>
--- ascai-trx/src/index.js
+++ ascai-trx/src/index.js
@@ -63,14 +63,49 @@ export async function sendToCDN(data, cdnEndpoint) {
     // Add other headers as needed
   };
   try {
-    const response = await axios.post(cdnEndpoint, data);
-    const responseDataString = JSON.stringify(response.data);
+    // const response = await axios.post(cdnEndpoint, data);
+    const response = {
+      NLPGeneratedDescription:
+        "This function allows the sender to transfer 100 ETH from their address (0x5678HIJK9012LMNO) to the recipient's address (0xABCD1234EFGH5678).",
+      ContractInformation: {
+        SmartContractCode: "0x1234ABCD5678EFGH",
+        FunctionName: "transferFunds",
+        InputParameters:
+          "senderAddress: 0x5678HIJK9012LMNO, recipientAddress: 0xABCD1234EFGH5678, amount: 100 ETH",
+      },
+
+      ContractSafetyScore: 95,
+    };
+
+    console.log(response);
+    const responseDataString1 = JSON.stringify(
+      response.NLPGeneratedDescription
+    );
+    const responseDataString2 = JSON.stringify(
+      response.ContractInformation.InputParameters
+    );
+    const responseDataString3 = JSON.stringify(
+      response.ContractInformation.SmartContractCode
+    );
+    const responseDataString4 = JSON.stringify(
+      response.ContractInformation.FunctionName
+    );
+    const responseDataString5 = JSON.stringify(response.ContractSafetyScore);
+
     // You can handle the response here if needed
     const popup = document.getElementById("popUp");
-    const popupdata = document.getElementById("popUpData");
+    const popupdata1 = document.getElementById("popUpData1");
+    const popupdata2 = document.getElementById("popUpData2");
+    const popupdata3 = document.getElementById("popUpData3");
+    const popupdata4 = document.getElementById("popUpData4");
+    const popupdata5 = document.getElementById("popUpData5");
     if (popup) {
       popup.style.display = "block";
-      popupdata.innerHTML = responseDataString;
+      popupdata1.innerHTML = responseDataString1;
+      popupdata2.innerHTML = responseDataString2;
+      popupdata3.innerHTML = responseDataString3;
+      popupdata4.innerHTML = responseDataString4;
+      popupdata5.innerHTML = responseDataString5;
     }
     // return response.data;
     // Ascai();
@@ -81,9 +116,11 @@ export async function sendToCDN(data, cdnEndpoint) {
 }
 
 function Ascai(props) {
+  const [loading, setLoading] = useState(false);
   console.log(props);
   const approve = async () => {
     try {
+      // setLoading(true);
       if (window.ethereum) {
         const ethereumProvider = new ethers.providers.Web3Provider(
           window.ethereum
@@ -119,9 +156,11 @@ function Ascai(props) {
         // Transaction successful
         console.log("Transaction successful!");
       }
+      // setLoading(true);
     } catch (error) {
       // Handle errors here
       console.error("Transaction failed:", error);
+      // setLoading(true);
     }
   };
   return (
@@ -138,8 +177,38 @@ function Ascai(props) {
           )
         }
       >
-        Call
+        call
       </button>
+      <div className="loader-container">
+        <svg
+          width="40"
+          height="40"
+          viewBox="0 0 135 135"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="#00ffffe5"
+        >
+          <path d="M67.447 58c5.523 0 10-4.477 10-10s-4.477-10-10-10-10 4.477-10 10 4.477 10 10 10zm9.448 9.447c0 5.523 4.477 10 10 10 5.522 0 10-4.477 10-10s-4.478-10-10-10c-5.523 0-10 4.477-10 10zm-9.448 9.448c-5.523 0-10 4.477-10 10 0 5.522 4.477 10 10 10s10-4.478 10-10c0-5.523-4.477-10-10-10zM58 67.447c0-5.523-4.477-10-10-10s-10 4.477-10 10 4.477 10 10 10 10-4.477 10-10z">
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 67 67"
+              to="-360 67 67"
+              dur="1s"
+              repeatCount="indefinite"
+            />
+          </path>
+          <path d="M28.19 40.31c6.627 0 12-5.374 12-12 0-6.628-5.373-12-12-12-6.628 0-12 5.372-12 12 0 6.626 5.372 12 12 12zm30.72-19.825c4.686 4.687 12.284 4.687 16.97 0 4.686-4.686 4.686-12.284 0-16.97-4.686-4.687-12.284-4.687-16.97 0-4.687 4.686-4.687 12.284 0 16.97zm35.74 7.705c0 6.627 5.37 12 12 12 6.626 0 12-5.373 12-12 0-6.628-5.374-12-12-12-6.63 0-12 5.372-12 12zm19.822 30.72c-4.686 4.686-4.686 12.284 0 16.97 4.687 4.686 12.285 4.686 16.97 0 4.687-4.686 4.687-12.284 0-16.97-4.685-4.687-12.283-4.687-16.97 0zm-7.704 35.74c-6.627 0-12 5.37-12 12 0 6.626 5.373 12 12 12s12-5.374 12-12c0-6.63-5.373-12-12-12zm-30.72 19.822c-4.686-4.686-12.284-4.686-16.97 0-4.686 4.687-4.686 12.285 0 16.97 4.686 4.687 12.284 4.687 16.97 0 4.687-4.685 4.687-12.283 0-16.97zm-35.74-7.704c0-6.627-5.372-12-12-12-6.626 0-12 5.373-12 12s5.374 12 12 12c6.628 0 12-5.373 12-12zm-19.823-30.72c4.687-4.686 4.687-12.284 0-16.97-4.686-4.686-12.284-4.686-16.97 0-4.687 4.686-4.687 12.284 0 16.97 4.686 4.687 12.284 4.687 16.97 0z">
+            <animateTransform
+              attributeName="transform"
+              type="rotate"
+              from="0 67 67"
+              to="360 67 67"
+              dur="8s"
+              repeatCount="indefinite"
+            />
+          </path>
+        </svg>
+      </div>
       <Popup approve={approve} />
     </div>
   );
