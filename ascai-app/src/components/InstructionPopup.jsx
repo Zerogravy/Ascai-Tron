871bc7cca55cd8e005c55b0c1b4a8dda7215b0af
Zerogravy <zerogravityexplorer@gmail.com>
2023-10-10 14:29:09 +0530

    updated link

--- ascai-app/src/components/InstructionPopup.jsx
+++ ascai-app/src/components/InstructionPopup.jsx
@@ -28,13 +28,17 @@ function InstructionPopup(props) {
           <li>
             Register yourself and explore the integrated package features by
             visiting the following link:
-            <a href="https://ascai-tron-t2he.vercel.app/">Testing Site</a>
+            <a href="https://ascai-tron-t2he.vercel.app/" target="_blank">
+              Testing Site
+            </a>
           </li>
           <li>Follow the registration process on the testing site.</li>
           <li>
             Alternatively, you can also test it on this site with a simple
             function call:
-            <a href="https://ascai-tron-zcng.vercel.app/">Test Site</a>
+            <a href="https://ascai-tron-zcng.vercel.app/" target="_blank">
+              Test Site
+            </a>
           </li>
         </div>
         <br></br>
